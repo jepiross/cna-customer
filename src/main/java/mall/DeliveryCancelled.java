f64c33cb01eca2c40867aa769eff54376cb3dd88
jepiross <jepiross@naver.com>
2020-09-25 11:36:00 +0900

    1st commit..

---
+++ src/main/java/mall/DeliveryCancelled.java
@@ -0,0 +1,30 @@
+package mall;
+
+public class DeliveryCancelled extends AbstractEvent {
+
+    private Long id;
+    private Long orderId;
+    private String staus;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+    public String getStaus() {
+        return staus;
+    }
+
+    public void setStaus(String staus) {
+        this.staus = staus;
+    }
+}
